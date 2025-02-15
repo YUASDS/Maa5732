27698b64546593a1aea7b0ce33fb84c98dc2592d
YUASDS <1787569211@qq.com>
2025-02-15 18:39:54 +0800

    添加log

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
-
+import datetime
 from loguru import logger
 from PySide6.QtWidgets import QApplication
 
 from src.ui.ui_controller import MyWidget
+
+now_time = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+logger.add(f"assets/logs/{now_time}.log")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
